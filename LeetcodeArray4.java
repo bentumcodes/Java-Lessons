cd2c81389a631f9edf7ea7e89b49d43ad51522c7
Bright Bentum <brillybenny@Brights-MacBook-Pro.local>
2026-01-29 17:49:49 +0900

    update on lesson 4

--- LeetcodeArray4.java
+++ LeetcodeArray4.java
@@ -2,10 +2,10 @@ public class LeetcodeArray4 {
     public static void main(String[] args){
         //Check who is wealthier
 
-        int[][] arr = new int[][]{{1,5},{7,3},{3,5}};
+        int[][] arr = new int[][]{{1,12},{7,14},{13,5}};
 
         //print out arrays
-       
+       int cusID=-1;
         int maxNo =0;
         //System.out.println("The Wealthiest amount is: ");
         for (int i =0; i < arr.length;i++){
@@ -13,15 +13,17 @@ public class LeetcodeArray4 {
             for (int j = 0; j < arr[i].length;j++){
 
                 sum += arr[i][j];
-            
         }
-        System.out.println("Customer "+(i+1)+":" +sum);
+        System.out.println("Customer "+(i+1)+"=> " +sum);
+        
         if ( sum > maxNo){
             maxNo = sum;
+            cusID = i + 1;
+        
         }
 }
         System.out.println();
-        System.out.println("The maximum number is: "+maxNo);
+        System.out.println("The Wealthier Customer is "+cusID+ " with number : "+maxNo);
     }
 
 }
